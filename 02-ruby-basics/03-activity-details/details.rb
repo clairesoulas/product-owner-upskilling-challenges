318f146b1356dcb62158f0820755117c9ed45a11
Claire Soulas <csoulas@lucca.fr>
2019-10-09 14:12:04 +0000

    Display activity theatre

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,15 @@
 activity = {
-  # TODO: gather the characteristics of your preferred activity
+  "name" => "Atelier theatre", #String
+  "category" => "Cultural", #String
+  "city" => "Nantes", #String
+  "duration" => 120, #Integer
+  "participants" => 12, #Integer
+  "price" => 20 #Integer
 }
 
 # TODO: your code goes here
+puts "#{activity["name"]} [#{activity["category"].upcase}]"
+puts "Located in #{activity["city"]}"
+puts "Duration of #{activity["duration"]} minutes"
+puts "For #{activity["participants"]} participants"
+puts "Charged #{activity["price"]}€ per participant"
