c02eb87b13d751298764197da54fbe9bff3ba0f3
Claire Soulas <csoulas@lucca.fr>
2019-10-09 13:31:05 +0000

    Request birth year and calculate age

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -4,3 +4,9 @@ name = gets.chomp # asks user to type something in the Terminal
 puts "Ok got it!"
 
 # TODO: your code goes here
+puts "What is your birth year?"
+birth_year = gets.chomp.to_i
+
+age = 2019 - birth_year
+
+puts "Thanks. So I guess you're #{age} years old."
