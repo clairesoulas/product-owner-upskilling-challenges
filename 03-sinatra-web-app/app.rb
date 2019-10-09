644ec559ae643a96200db8b16e3f80e1f822bf78
Claire Soulas <csoulas@lucca.fr>
2019-10-09 16:29:00 +0000

    Viewing list of static activities with Sinatra

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -7,6 +7,30 @@ enable :static
 get "/" do
   @activities = [
     # TODO: gather a few activities
+    {
+      "name" => "Atelier theatre", #String
+      "category" => "Cultural", #String
+      "city" => "Nantes", #String
+      "duration" => 120, #Integer
+      "participants" => 12, #Integer
+      "price" => 20 #Integer
+    },
+    {
+      "name" => "Escape game", #String
+      "category" => "Adventure", #String
+      "city" => "Nantes", #String
+      "duration" => 75, #Integer
+      "participants" => 6, #Integer
+      "price" => 20 #Integer
+    },
+    {
+      "name" => "Stage de survie", #String
+      "category" => "Adventure", #String
+      "city" => "La forêt", #String
+      "duration" => 180, #Integer
+      "participants" => 6, #Integer
+      "price" => 150 #Integer
+    }
   ]
 
   erb :index
