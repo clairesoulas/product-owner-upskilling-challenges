7c679b7af9f7bd65122bc80b5ffdbf4a1ec1f947
Claire Soulas <csoulas@lucca.fr>
2019-10-09 13:46:38 +0000

    Possibility to add a category and list them all

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,15 @@
-categories = [
-  # TODO: gather the categories of activity
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"
 ]
 
 # TODO: your code goes here
+puts "What extra category would you like to add?"
+extra_category = gets.chomp
+categories = categories << extra_category
+categories = categories.sort
+puts"Thank you, so currently, the categories are:"
+categories.each do |category|
+  puts "#{category}"
+end
+
+size_categories = categories.size
+puts "Total number of categories: #{size_categories}"
