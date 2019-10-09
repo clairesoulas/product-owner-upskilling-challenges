083cc3cd3e1324f6c9845876d331815d2c4020b0
Claire Soulas <csoulas@lucca.fr>
2019-10-09 14:54:27 +0000

    Check if age is over 100

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -10,6 +10,8 @@ birth_year = gets.chomp.to_i
 age = 2019 - birth_year
 if age <= 0
   puts "Sorry that would be a real strange birth year."
+elsif age > 100
+  puts "Whoo you must be really old."
 else
   puts "Thanks. So I guess you're #{age} years old."
 end
