0dfd572ff25f33b48c4553dceece3ba10c253fa7
Claire Soulas <csoulas@lucca.fr>
2019-10-11 14:10:57 +0000

    Display number of users for a given role

--- 05-api-consumption/01-ruby-program/users_by_role.rb
+++ 05-api-consumption/01-ruby-program/users_by_role.rb
@@ -2,8 +2,20 @@ require "json"
 require "rest-client"
 
 puts "What is the role of the users you're looking for? (e.g. Figgo - Manager)"
-role = gets.chomp # asks user to type something in the Terminal
+role_name = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it! Let's search for the users..."
 
 # TODO: your code goes here
+
+token = "18afcfb8-697b-4742-b726-418269485cee"
+headers = {"Authorization" => "Lucca application=#{token}"}
+url = "https://wagon.ilucca-demo.net/api/v3/roles"
+
+response = RestClient.get(url, "params" => {"fields" => "Name,UsersCount,Users","Name" => "like,#{role_name}"}, "Authorization" => "Lucca application=#{token}")
+
+users_of_role = JSON.parse(response.body)
+
+count = users_of_role["data"]["items"][0]["usersCount"]
+
+puts "Total: #{count} users found"
