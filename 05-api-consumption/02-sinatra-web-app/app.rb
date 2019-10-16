485d0d27a16ae4c62391721ed3bcaed9806c1e3c
Claire Soulas <csoulas@lucca.fr>
2019-10-16 14:40:07 +0000

    Search by category & name + remember user parameters

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -8,11 +8,34 @@ require "sinatra/reloader" if development?
 enable :static
 
 get "/" do
+  # Get all activites, and retrieve categories
   url = "https://team-building-api.cleverapps.io/v2/activities"
-  response = RestClient.get(url, "params"=>{"city"=>"#{params["location"]}"})
+  response = RestClient.get(url)
+  all_activities = JSON.parse(response.body)
+
+  all_activities = all_activities["activities"]
+  @categories=[]
+
+  all_activities.each do |activity|
+    @categories << activity["category"]
+  end
+  @categories=@categories.uniq
+
+  url = "https://team-building-api.cleverapps.io/v2/activities"
+  response = RestClient.get(url, "params"=>{"city"=> params["location"], "search" => "#{params[:name]}", "category" => "#{params[:category]}"})
+
   activities = JSON.parse(response.body)
 
+  @selected_category = params[:category]
+
+  # Input search is empty or reusing value provided by user
+  if params[:name] == nil
+    then params[:name] = ""
+  end
+  @name_searched = params[:name]
+
   @activities = activities["activities"]
+
   erb :index
 end
 
@@ -23,14 +46,4 @@ get "/activities/:activity_id" do
 
   @activity = activities["activity"]
   erb :show
-end
-
-post "/" do
-  url = "https://team-building-api.cleverapps.io/v2/activities"
-  response = RestClient.get(url, "params" => {"search" => "#{params[:name]}", "category" => "#{params[:categories]}"})
-  activities = JSON.parse(response.body)
-
-  @activities = activities["activities"]
-
-  erb :index
 end
