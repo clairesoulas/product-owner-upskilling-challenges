1f2e10e9bdc9120bb99ec536b5f51fd531120bcd
Claire Soulas <csoulas@lucca.fr>
2019-10-16 08:09:52 +0000

    Call endpoint for details of an activity when id is provided in URL

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -13,6 +13,17 @@ get "/" do
   response = RestClient.get(url)
   activities = JSON.parse(response.body)
 
-  @activities = activities["activities"]# TODO: retrieve all the activities from the API
+  @activities = activities["activities"]
   erb :index
+end
+
+get "/:activity_id" do
+  #activity_id = "25"
+  url = "https://team-building-api.cleverapps.io/v2/activities/#{params["activity_id"]}"
+  puts "#{url}"
+  response = RestClient.get(url)
+  activities = JSON.parse(response.body)
+
+  @activities = activities["activities"]
+  #erb :index
 end
